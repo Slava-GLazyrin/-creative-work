801248d8f8151d83f9723ab9be57606d59b033eb
GlazyrinVM <92156718+Slava-GLazyrin@users.noreply.github.com>
2022-05-17 00:22:32 +0500

    Add files via upload

---
+++ Коммивояжер/Main.cpp
@@ -0,0 +1,23 @@
+﻿#include "Graph.h"
+
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "ru");
+
+
+	glutInit(&argc, argv);
+
+	setGraph();
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WinW, WinH);
+	glutCreateWindow("Graph");
+	glutDisplayFunc(display);
+	glutReshapeFunc(change);
+	glutMouseFunc(mouseClick);
+
+	glutMainLoop();
+
+	return 0;
+}
